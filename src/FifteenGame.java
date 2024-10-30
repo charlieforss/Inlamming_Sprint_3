7a4f4090ec92c14a73bf307dbc01cda9512be963
Charlie <charlieforss@gmail.com>
2024-10-30 17:12:23 +0100

    redo

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -1,6 +1,17 @@
-import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.*;
 
+public class FifteenGame extends JFrame implements ActionListener {
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+    public void main(String[] args) {
+        new FifteenGame();
+    }
+}
