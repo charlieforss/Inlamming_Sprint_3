e3f79c43d089bac275391ac39b07c2275cf7c843
Charlie <charlieforss@gmail.com>
2024-10-30 17:14:25 +0100

    panelwork

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -17,6 +17,24 @@ public class FifteenGame extends JFrame implements ActionListener {
     JButton button5 = new JButton("");
     ArrayList<JButton> buttons;
 
+    public FifteenGame() {
+        buttons = new ArrayList<>();
+
+        for (int i = 1; i <= 15; i++) {
+            JButton button = new JButton(String.valueOf(i));
+            button.addActionListener(this);
+            buttons.add(button);
+            p2.add(button);
+        }
+
+        setTitle("Test");
+        this.add(p);
+        p.setLayout(new BorderLayout());
+        p.add(p2, BorderLayout.CENTER);
+        p.add(p3, BorderLayout.SOUTH);
+        p2.setLayout(new GridLayout(4, 4));
+        p3.setLayout(new FlowLayout());
+
 
 
 
