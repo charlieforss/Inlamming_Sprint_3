4d704fe6475737428ff225af99cc3099ed0bfa68
Charlie <charlieforss@gmail.com>
2024-10-30 17:11:15 +0100

    redo

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -3,22 +3,4 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class FifteenGame {
 
-    public class fifteenGame extends JFrame implements ActionListener {
-
-        Panel p = new Panel();
-        Panel p2 = new Panel();
-        Panel p3 = new Panel();
-
-
-        public static void main(String[] args) {
-
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-
-        }
-    }
-}
