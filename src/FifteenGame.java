6a490b95136ad882fd5c37cffb2c2bee6f658b29
Charlie <charlieforss@gmail.com>
2024-10-30 17:21:24 +0100

    forgotten stuff

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -36,12 +36,73 @@ public class FifteenGame extends JFrame implements ActionListener {
         p3.setLayout(new FlowLayout());
     }
 
+    private void newGame(){
+        p2.removeAll();
+        Collections.shuffle(buttons);
+        for (JButton button : buttons) {
+            p2.add(button);
+        }
+        p2.revalidate();
+        p2.repaint();
+    }
 
+    private void exit(){
+        System.exit(0);
+    }
+
+    private void fusk() {
+        JButton button15 = null;
+        for (JButton btn : buttons) {
+            if (btn.getText().equals("15")) {
+                button15 = btn;
+                break;
+            }
+        }
+        if (button15 != null) {
+            Collections.swap(buttons, buttons.indexOf(button5), buttons.indexOf(button15));
+            p2.removeAll();
+            for (JButton button : buttons) {
+                p2.add(button);
+            }
+            p2.revalidate();
+            p2.repaint();
+        }
+    }
+    private boolean isNextToEmpty(int index1, int index2) {
+        int row1 = index1 / 4;
+        int col1 = index1 % 4;
+        int row2 = index2 / 4;
+        int col2 = index2 % 4;
+        return (Math.abs(row1 - row2) + Math.abs(col1 - col2)) == 1;
+    }
+
+    private boolean done() {
+        for (int i = 0; i < 15; i++) {
+            if (!buttons.get(i).getText().equals(String.valueOf(i + 1))) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        JButton pressedButton = (JButton) e.getSource();
+        int preesedIndex = buttons.indexOf(pressedButton);
+        int emptyButtonIndex = buttons.indexOf(button5);
 
+        if (isNextToEmpty(preesedIndex, emptyButtonIndex)) {
+            Collections.swap(buttons, preesedIndex, emptyButtonIndex);
+            p2.removeAll();
+            for (JButton button : buttons) {
+                p2.add(button);
+            }
+            p2.revalidate();
+            if (done()) {
+                JOptionPane.showMessageDialog(this, "Grattis, du vann!");
+            }
+        }
     }
     public void main(String[] args) {
         new FifteenGame();
