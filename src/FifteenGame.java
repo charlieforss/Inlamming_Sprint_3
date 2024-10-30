05ec8281c3bfef481de260fc1bd7728e2b483891
Charlie <charlieforss@gmail.com>
2024-10-30 16:56:41 +0100

    Start

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -6,6 +7,9 @@ public class FifteenGame {
 
     public class fifteenGame extends JFrame implements ActionListener {
 
+        Panel p = new Panel();
+        Panel p2 = new Panel();
+        Panel p3 = new Panel();
 
 
         public static void main(String[] args) {
