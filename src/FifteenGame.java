4b44bf4870c89f8fdb9f26ad8c419c7793263f2c
Charlie <charlieforss@gmail.com>
2024-10-30 17:27:00 +0100

    panel arbete

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -7,8 +7,21 @@ import javax.swing.*;
 
 public class FifteenGame extends JFrame implements ActionListener {
 
+    Panel P = new Panel();
+    Panel P2 = new Panel();
+    Panel P3 = new Panel();
 
+    JButton button1 = new JButton("Start/Restart");
+    JButton button2 = new JButton("Exit");
+    JButton button3 = new JButton("CLICKA INTE");
+    JButton button4 = new JButton("");
 
+    ArrayList<JButton> buttons;
+
+    public FifteenGame() {
+        buttons = new ArrayList<>();
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
